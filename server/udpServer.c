12ad1a19d13764698bdac1696f573a58d11adbbd
redbaron23 <pato>
2020-08-28 20:36:17 -0300

    Loop

--- server/udpServer.c
+++ server/udpServer.c
@@ -1,84 +1,85 @@
 
-// Server side implementation of UDP client-server model 
-#include <stdio.h> 
-#include <stdlib.h> 
-#include <unistd.h> 
-#include <string.h> 
-#include <sys/types.h> 
-#include <sys/socket.h> 
-#include <arpa/inet.h> 
-#include <netinet/in.h> 
-  
-#define PORT     8080 
-#define MAXLINE 1024 
-  
-// Driver code 
-int main() { 
-    int sockfd; 
-    char buffer[MAXLINE]; 
+// Server side implementation of UDP client-server model
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+
+#define PORT     8080
+#define MAXLINE 1024
+
+// Driver code
+int main() {
+    int sockfd;
+    char buffer[MAXLINE];
     int status;
-  FILE *fp;
-  char path[1035];
+    FILE *fp;
+    char path[1035];
 
     char *res;
-    char *hello = "Hello from server"; 
-    struct sockaddr_in servaddr, cliaddr; 
-      
-    // Creating socket file descriptor 
-    if ( (sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0 ) { 
-        perror("socket creation failed"); 
-        exit(EXIT_FAILURE); 
-    } 
-      
-    memset(&servaddr, 0, sizeof(servaddr)); 
-    memset(&cliaddr, 0, sizeof(cliaddr)); 
-      
-    // Filling server information 
-    servaddr.sin_family    = AF_INET; // IPv4 
-    servaddr.sin_addr.s_addr = INADDR_ANY; 
-    servaddr.sin_port = htons(PORT); 
-      
-    // Bind the socket with the server address 
-    if ( bind(sockfd, (const struct sockaddr *)&servaddr,  
-            sizeof(servaddr)) < 0 ) 
-    { 
-        perror("bind failed"); 
-        exit(EXIT_FAILURE); 
-    } 
-      
-    int len, n; 
-  
-    len = sizeof(cliaddr);  //len is value/resuslt 
-  
-    n = recvfrom(sockfd, (char *)buffer, MAXLINE,  
-                MSG_WAITALL, ( struct sockaddr *) &cliaddr, 
-                &len); 
-    buffer[n] = '\0'; 
+    char *hello = "Hello from server";
+    struct sockaddr_in servaddr, cliaddr;
 
-    
-    printf("Client execute: %s\n", buffer); 
+    // Creating socket file descriptor
+    if ( (sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0 ) {
+        perror("socket creation failed");
+        exit(EXIT_FAILURE);
+    }
+
+    memset(&servaddr, 0, sizeof(servaddr));
+    memset(&cliaddr, 0, sizeof(cliaddr));
+
+    // Filling server information
+    servaddr.sin_family    = AF_INET; // IPv4
+    servaddr.sin_addr.s_addr = INADDR_ANY;
+    servaddr.sin_port = htons(PORT);
+
+    // Bind the socket with the server address
+    if ( bind(sockfd, (const struct sockaddr *)&servaddr,
+              sizeof(servaddr)) < 0 )
+    {
+        perror("bind failed");
+        exit(EXIT_FAILURE);
+    }
+
+    int len, n;
+
+    len = sizeof(cliaddr);  //len is value/resuslt
+  while (1){
+    n = recvfrom(sockfd, (char *)buffer, MAXLINE,
+                 MSG_WAITALL, ( struct sockaddr *) &cliaddr,
+                 &len);
+    buffer[n] = '\0';
+
+
+    printf("Client execute: %s\n", buffer);
     status = system(buffer);
 
-  /* Open the command for reading. */
-  fp = popen(buffer, "r");
-  if (fp == NULL) {
-    printf("Failed to run command\n" );
-    exit(1);
+    /* Open the command for reading. */
+    fp = popen(buffer, "r");
+    if (fp == NULL) {
+        printf("Failed to run command\n" );
+        exit(1);
+    }
+
+    /* Read the output a line at a time - output it
+    while (fgets(path, sizeof(path), fp) != NULL) {
+      printf("%s", path);
+    }*/
+
+
+    /* close */
+    printf("Aca %s\n",fp);
+    sendto(sockfd, (const char *)fp, strlen( (const char *)fp ),
+           MSG_CONFIRM, (const struct sockaddr *) &cliaddr,
+           len);
+    printf("Output sent.\n");
+
+    pclose(fp);
   }
-
-  /* Read the output a line at a time - output it
-  while (fgets(path, sizeof(path), fp) != NULL) {
-    printf("%s", path);
-  }*/
-
-
-  /* close */
-
-    sendto(sockfd, (const char *)fp, strlen( (const char *)fp ),  
-        MSG_CONFIRM, (const struct sockaddr *) &cliaddr, 
-            len); 
-    printf("Output sent.\n");  
-      
-  pclose(fp);
-    return 0; 
-} 
+    return 0;
+}
